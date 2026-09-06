947b0d561992f980436ab5ca50460ebd2035018b
jaosullivan <johnaosullivan@hotmail.com>
2026-09-06 22:40:51 +0800

    chore(deploy): terraform VPC module (single NAT)

---
+++ deploy/terraform/vpc.tf
@@ -0,0 +1,32 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+locals {
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project}-${var.environment}"
+  cidr = var.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = [for i, az in local.azs : cidrsubnet(var.vpc_cidr, 4, i)]
+  public_subnets  = [for i, az in local.azs : cidrsubnet(var.vpc_cidr, 4, i + 8)]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true # cost: one NAT
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
